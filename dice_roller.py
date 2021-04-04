2bc6633b4ed5672cca548ce21fdc9785f53cf6d8
Chen <Ren.Chen2@ontario.ca>
2021-04-03 21:44:25 -0400

    side and round

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 3
+  dice_rolls = int(input("how many round do you want roll?"))
+  dice_side = int(input("how many side does dice has?"))
   dice_sum = 0
   for i in range(1, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_side)
+    if roll == 1:
+      print(f"you rolled {roll} cirtical fail")
+    elif roll == 6:
+      print(f"you rolled {roll}! cirtical sucess")
+    else:
+      print(f'You rolled a die {roll}')
     dice_sum += roll
-    print(f'You rolled a die {roll}')
   print(f'total dice_sum {dice_sum}')
 if __name__== "__main__":
   main()
